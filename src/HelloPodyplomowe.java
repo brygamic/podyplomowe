cdfe631b963e6f4e092ba74a89f1f73fefeb8f09
m@m <m@m>
2023-10-08 16:04:47 +0200

    Initial commit

---
+++ src/HelloPodyplomowe.java
@@ -0,0 +1,6 @@
+public class HelloPodyplomowe {
+    public static void main(String[] args) {
+        String student= "Michał";
+        System.out.println("Cześć "+ student + " !!!!"+ " Witamy na studiach podyplomowych WSZiB");
+    }
+}
